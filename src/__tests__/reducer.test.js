147a8d2c920766a9346528cca5003643e49ad6d3
Daniel M. Matongo <m_matongo@icloud.com>
2021-10-13 16:20:59 +0200

    add new required tests

--- src/__tests__/reducer.test.js
+++ src/__tests__/reducer.test.js
@@ -1,6 +1,31 @@
 import covidReducer from '../redux/reducers/covid';
 import dataReducer from '../redux/reducers/data';
 
+const mockStore = {
+  covid: {
+    covidCountries: [
+      {
+        country: 'Nigeria',
+        cases: '1,000,000',
+        deaths: '1,000',
+        recovered: '1,000',
+        active: '1,000',
+      },
+    ],
+  },
+  covidData: {
+    covidData: [
+      {
+        country: 'Nigeria',
+        cases: '1,000,000',
+        deaths: '1,000',
+        recovered: '1,000',
+        active: '1,000',
+      },
+    ],
+  },
+};
+
 describe('Test the Data and Covid reducers', () => {
   test('Return the initial state of the covid reducer.', () => {
     expect(covidReducer(undefined, {})).toEqual({
@@ -26,6 +51,71 @@ describe('Test the Data and Covid reducers', () => {
     const GET_DETAILS = 'covid_data/detailsslice/GET_DETAILS';
     expect(dataReducer(undefined, { type: GET_DETAILS })).toEqual({
       covidData: [],
+      pending: true,
+    });
+  });
+});
+
+describe('Test the Data and Covid reducers', () => {
+  test('Test Data reducer with mock Data.', () => {
+    const GET_COVID = 'covid/covidslice/GET_COVID';
+    const store = mockStore;
+    const action = {
+      type: GET_COVID,
+      payload: {
+        covidCountries: [
+          {
+            country: 'Nigeria',
+            cases: '1,000,000',
+            deaths: '1,000',
+            recovered: '1,000',
+            active: '1,000',
+          },
+        ],
+        pending: false,
+      },
+    };
+    expect(covidReducer(store.covid, action)).toEqual({
+      covidCountries: [
+        {
+          country: 'Nigeria',
+          cases: '1,000,000',
+          deaths: '1,000',
+          recovered: '1,000',
+          active: '1,000',
+        },
+      ],
+      pending: true,
+    });
+  });
+  test('Test Covid reducer with mock Data.', () => {
+    const GET_COVID = 'covid/covidslice/GET_COVID';
+    const store = mockStore;
+    const action = {
+      type: GET_COVID,
+      payload: {
+        covidCountries: [
+          {
+            country: 'Nigeria',
+            cases: '1,000,000',
+            deaths: '1,000',
+            recovered: '1,000',
+            active: '1,000',
+          },
+        ],
+        pending: false,
+      },
+    };
+    expect(covidReducer(store.covid, action)).toEqual({
+      covidCountries: [
+        {
+          country: 'Nigeria',
+          cases: '1,000,000',
+          deaths: '1,000',
+          recovered: '1,000',
+          active: '1,000',
+        },
+      ],
       pending: true,
     });
   });
